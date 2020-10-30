924a0a57f686a9420203cebb56e03d7557b7cb7f
Tobias <tobias@vire.co>
2020-10-30 15:15:55 +0100

    Added image to tags

--- src/cryogen_core/compiler.clj
+++ src/cryogen_core/compiler.clj
@@ -156,6 +156,7 @@
              :parsed-archive-group    (.parse archive-fmt formatted-group)
              :uri                     (page-uri file-name :post-root-uri config)
              :tags                    (set (:tags page-meta))
+             :image                   (:image page-meta)
              :klipse/global           (:klipse config)
              :klipse/local            (:klipse page-meta)})
           (add-toc config)))))
