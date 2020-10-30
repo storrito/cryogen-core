14fc896c1d4677dd5ea76bb253d6ce2dd7913e75
Tobias <tobias@vire.co>
2020-10-30 17:57:01 +0100

    fixed image path

--- src/cryogen_core/rss.clj
+++ src/cryogen_core/rss.clj
@@ -6,7 +6,7 @@
   (:import java.util.Date))
 
 
-(defn posts-to-items [^String site-url posts]
+(defn posts-to-items [{:keys [site-url blog-prefix]} posts]
   (map
     (fn [{:keys [uri title content-dom date enclosure author description image]}]
       (let [site-url (if (.endsWith site-url "/") (apply str (butlast site-url)) site-url)
@@ -18,7 +18,7 @@
                  :author      author
                  :pubDate     date}
                 image
-                (assoc :thumbnail (str site-url image))
+                (assoc :thumbnail (str site-url blog-prefix image))
                 enclosure
                 (assoc :enclosure enclosure))))
     posts))
@@ -31,7 +31,7 @@
               :link          (:site-url config)
               :description   (:description config)
               :lastBuildDate (Date.)})
-    (posts-to-items (:site-url config) posts)))
+    (posts-to-items config posts)))
 
 (defn make-filtered-channels [{:keys [rss-filters blog-prefix] :as config} posts-by-tag]
   (doseq [filter rss-filters]
